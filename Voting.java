059f4ba6a9f86289c5da78d493ac8cc5b7441bc8
Francinaldo Almeida <fap_sb@globomail.com>
2016-03-28 13:54:32 -0500

    Refactoring

--- Voting.java
+++ Voting.java
@@ -8,6 +8,10 @@ import java.io.PrintWriter;
 public class Voting
 {
 	private JPanel panel;
+	private String nameA;
+	private String nameB;
+	private String nameC;
+	private String nameD;
 	private JButton candidateA;
 	private JButton candidateB;
 	private JButton candidateC;
@@ -21,18 +25,25 @@ public class Voting
 	public Voting()
 	{
 		panel = new JPanel();
-		candidateA = new JButton("A");
-		candidateB = new JButton("B");
-		candidateC = new JButton("C");
-		candidateD = new JButton("D");
-		firstName = new JTextField(6);
-		lastName = new JTextField(6);
+		nameA = "SpongeBob";
+		nameB = "Patrick";
+		nameC = "Squidward";
+		nameD = "Mr. Krabs";
+		candidateA = new JButton(nameA);
+		candidateB = new JButton(nameB);
+		candidateC = new JButton(nameC);
+		candidateD = new JButton(nameD);
+		firstName = new JTextField(9);
+		lastName = new JTextField(9);
 		label1 = new JLabel("First Name:");
 		label2 = new JLabel("Last Name:");
-		resultLabel = new JLabel("(The conversion will be shown here)");
+		resultLabel = new JLabel("(Chose one of the candidates)");
 		
 		//Load the listener
 		candidateA.addActionListener(VoteForA());
+		candidateB.addActionListener(VoteForB());
+		candidateC.addActionListener(VoteForC());
+		candidateD.addActionListener(VoteForD());
 		
 		//load the panel
 		panel.add(label1);
@@ -51,33 +62,75 @@ public class Voting
 		return (panel);
 	}
 	
+	private void HandleVote(String candidate){
+		String fName = firstName.getText();
+		String lName = lastName.getText();
+
+		if (!fName.equals("") && !lName.equals("")) {
+			try {
+				File file = new File(lName+"_"+fName+"_ballot.txt");
+				if(file.createNewFile()){
+					PrintWriter writer = new PrintWriter(lName+"_"+fName+"_ballot.txt", "UTF-8");
+					writer.println(candidate);
+					writer.close();
+					resultLabel.setText("Thank you for your vote!");
+				} else {
+					resultLabel.setText("You already voted");
+				}
+			} catch (Exception ne) {
+				resultLabel.setText("Something went wrong.");
+			}
+		} else {
+			resultLabel.setText("Provide a Name and a Last Name");
+		}
+	}
+	
 	private ActionListener VoteForA()
 	{
 		ActionListener listener = new ActionListener()
 		{
 			public void actionPerformed(ActionEvent e)
 			{
-				String fName = firstName.getText();
-				String lName = lastName.getText();
-
-				if (!fName.equals("") && !lName.equals("")) {
-					try {
-						File file = new File(lName+"_"+fName+"_ballot.txt");
-						if(file.createNewFile()){
-							PrintWriter writer = new PrintWriter(lName+"_"+fName+"_ballot.txt", "UTF-8");
-							writer.println("Candidate A");
-							writer.close();
-							resultLabel.setText("Thank you for your vote!");
-						} else {
-							resultLabel.setText("You already voted");
-						}
-					} catch (Exception ne) {
-						resultLabel.setText("Something went wrong.");
-					}
-				} else {
-					resultLabel.setText("Provide a Name and a Last Name");
-				}
-
+				HandleVote(nameA);
+			}
+		};
+		
+		return listener;
+	}
+	
+	private ActionListener VoteForB()
+	{
+		ActionListener listener = new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				HandleVote(nameB);
+			}
+		};
+		
+		return listener;
+	}
+	
+	private ActionListener VoteForC()
+	{
+		ActionListener listener = new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				HandleVote(nameC);
+			}
+		};
+		
+		return listener;
+	}
+	
+	private ActionListener VoteForD()
+	{
+		ActionListener listener = new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				HandleVote(nameD);
 			}
 		};
 		
